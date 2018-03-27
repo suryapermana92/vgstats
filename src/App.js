aca19c5dc473290aba731c585a6d5b04b280e575
Surya <suryapermana92@gmail.com>
2018-03-28 03:03:41 +0700

    install vainglory and test code

--- src/App.js
+++ src/App.js
@@ -2,8 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Vainglory from 'vainglory';
+
+
 class App extends Component {
+  
+
+  getPlayerInfo(vainglory, playerNames) {
+    vainglory.players.getByName(playerNames).then((players) => {
+      if (players.errors || players.player) {
+        console.log(players)
+        return
+      };
+      players.player.forEach(player => {
+        console.log(player.id);
+        console.log(player.stats);
+      }).catch((errors) => {
+        console.log(errors);
+      });
+    })
+  }
   render() {
+    const options = {
+      host: 'https://api.dc01.gamelockerapp.com/shards/',
+      region: 'sg',
+      title: 'semc-vainglory',
+    }
+    const vainglory = new Vainglory('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2OWIyMWY0MC0xM2U2LTAxMzYtMWRkOS0wYTU4NjQ2MGE2MTciLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIyMTUyOTA3LCJwdWIiOiJzZW1jIiwidGl0bGUiOiJ2YWluZ2xvcnkiLCJhcHAiOiJnbG9yeXRlYW0iLCJzY29wZSI6ImNvbW11bml0eSIsImxpbWl0Ijo1MH0.JIopTQwcBMVBa2hUOMlqOm6Jfypk3Sm2bmdgVbqkqlI', options)
+    const playerNames = ['Electricat'];
+    
+    this.getPlayerInfo(vainglory, playerNames) 
+    
+    // this.callAPI()
+    console.log(options, vainglory)
     return (
       <div className="App">
         <header className="App-header">
